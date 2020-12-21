25a569564c384ab7de8925ed3ab96bede914ab4b
JParagreen <76440796+JParagreen@users.noreply.github.com>
2020-12-21 14:57:04 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,48 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        `)
+    control.waitMicros(10000000)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        # . . . #
+        . . . . .
+        `)
+    control.waitMicros(10000000)
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
 })
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . . . #
+        # . . # .
+        # . # . .
+        # # . . .
+        # # # # .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        # . . . .
+        . # . . #
+        . . # . #
+        . . . # #
+        . # # # #
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showIcon(IconNames.StickFigure)
+})
+basic.showString("" + (input.temperature()))
+basic.showString("Deg C")
